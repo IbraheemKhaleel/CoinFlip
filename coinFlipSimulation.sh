a8d830d4ea5b73a97a8226e9b0ccd85f5c56d80d
Ibraheem Khaleel <ikhaleelem@gmail.com>
2020-09-01 12:38:08 +0530

    added code for difference must be greater than 2 if it tied

---
+++ coinFlipSimulation.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+H=0
+T=0
+echo "Coin flip simulation till 21 times is running "
+while [[ $((H)) -lt 21 && $((T)) -lt 21 ]]
+do
+	rand=$((RANDOM%2))
+	if [[ $((rand)) -eq 0 ]]
+	then
+		echo "It is a head "
+		((H++))
+	else
+		echo "It is a tail "
+		((T++))
+	fi
+done
+if [[ $((H)) -eq 21 && $((T)) -eq 21 ]]
+then 
+	echo " It is a tie. Both reached 21 times "
+	while [[ $((H-T)) -ge 2 ]]
+	do
+		rand=$((RANDOM%2))
+        	if [[ $((rand)) -eq 0 ]]
+        	then
+                	echo "It is a head "
+                	((H++))
+        	else
+                	echo "It is a tail "
+                	((T++))
+        	fi
+	done
+	if [[ $((H)) -gt $((T)) ]]
+	then
+		echo "Head has reached $H times. Head won "
+	else
+		echo "Tail has reached $T times. Tail won "
+	fi
+elif [[ $((H)) -eq 21 ]]
+then
+        echo "Head has reached 21 times. Head won "
+else
+	echo "Tail has reached 21 time. Tails won"		
+fi
